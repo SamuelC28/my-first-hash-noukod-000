7b7e989435b3e7b03da6d31c9acd8fbd0d52a131
Samuel Cornet <samuel@noukod.com>
2018-09-21 17:49:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,10 +34,6 @@ def adding
   shipping_manifest["gun powder"] = 4
 
 
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+  return shipping_manifest
 
 end
