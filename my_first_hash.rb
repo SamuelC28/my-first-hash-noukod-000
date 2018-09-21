aca4a1edc16ebb8650aef029343aadc23b7b247a
Samuel Cornet <samuel@noukod.com>
2018-09-21 17:38:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-your_hash = {my_name => "Samuel Cornet", her_name => "Lisa Heather", his_name => "Jim Geissinger"}
+your_hash = {"my_name" => "Samuel Cornet", "her_name" => "Lisa Heather", "his_name" => "Jim Geissinger"}
 
 end
 
